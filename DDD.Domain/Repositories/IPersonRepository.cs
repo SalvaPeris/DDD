dcafcc792db044ae6cb0574e1340385333d172dc
Salva Peris <sperisgimeno@gmail.com>
2025-05-12 09:28:37 +0200

    Added Repository for Person.

---
+++ DDD.Domain/Repositories/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.ValueObjects;
+
+namespace DDD.Domain.Repositories
+{
+    public interface IPersonRepository
+    {
+        Task<Person> GetByIdAsync(PersonId id);
+        Task AddAsync(Person person);
+    }
+}
