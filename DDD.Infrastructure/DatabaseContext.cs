160630a666f9735d2a97c2cc2f58f373682fcf96
Salva Peris <sperisgimeno@gmail.com>
2025-05-12 09:28:56 +0200

    Added database context.

---
+++ DDD.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using DDD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDD.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Person>().OwnsOne(p => p.Name, conf =>
+            {
+                conf.Property(n => n.Value).HasColumnName("Name");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
