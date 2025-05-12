af182f39315f1abe5dad2b26e48ab4213565bf77
Salva Peris <sperisgimeno@gmail.com>
2025-05-12 09:33:33 +0200

    Added Person repository implementation.

---
+++ DDD.Infrastructure/PersonRepository.cs
@@ -0,0 +1,27 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Repositories;
+using DDD.Domain.ValueObjects;
+
+namespace DDD.Infrastructure
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public PersonRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Person> GetByIdAsync(PersonId id)
+        {
+            return await _context.Persons.FindAsync((Guid)id);
+        }
+
+        public async Task AddAsync(Person person)
+        {
+            await _context.Persons.AddAsync(person);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
